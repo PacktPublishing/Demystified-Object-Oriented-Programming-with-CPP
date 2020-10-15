1bafc1658b6e92b9fabb2d19a1f67dcdb520228f
DorothyRKirk <64913825+DorothyRKirk@users.noreply.github.com>
2020-10-15 17:49:01 -0400

    Add files via upload

---
+++ Chapter11/Chp11-Ex2.cpp
@@ -0,0 +1,230 @@
+// (c) Dorothy R. Kirk. All Rights Reserved.
+// Purpose: To illustrate registering a user defined function with terminate() using set_terminate(),
+// This function will be called instead of abort() 
+
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+
+using namespace std;
+
+const int MAX = 5;
+
+// AppSpecificTerminate is called in lieu of abort() since there is no matching catch block.
+void AppSpecificTerminate()
+{
+    cout << "Uncaught exception. Program terminating" << endl;
+    exit(1);
+}
+
+class Person
+{
+private: 
+    // data members
+    char *firstName;
+    char *lastName;
+    char middleInitial;
+    char *title;  // Mr., Ms., Mrs., Miss, Dr., etc.
+protected:
+    void ModifyTitle(const char *); 
+public:
+    Person();   // default constructor
+    Person(const char *, const char *, char, const char *);  
+    Person(const Person &);  // copy constructor
+    virtual ~Person();  // virtual destructor
+
+    // inline function definitions
+    const char *GetFirstName() { return firstName; }  
+    const char *GetLastName() { return lastName; }    
+    const char *GetTitle() { return title; } 
+    char GetMiddleInitial(){ return middleInitial; }
+
+    // Virtual functions will not be inlined since their 
+    // method must be determined at run time using v-table.
+    virtual void Print();
+    virtual void IsA();  
+    virtual void Greeting(const char *);
+};
+
+Person::Person()
+{
+    firstName = lastName = 0;  // NULL pointer
+    middleInitial = '\0';
+    title = 0;
+}
+
+Person::Person(const char *fn, const char *ln, char mi, 
+               const char *t)
+{
+    firstName = new char [strlen(fn) + 1];
+    strcpy(firstName, fn);
+    lastName = new char [strlen(ln) + 1];
+    strcpy(lastName, ln);
+    middleInitial = mi;
+    title = new char [strlen(t) + 1];
+    strcpy(title, t);
+}
+
+Person::Person(const Person &pers)
+{
+    firstName = new char [strlen(pers.firstName) + 1];
+    strcpy(firstName, pers.firstName);
+    lastName = new char [strlen(pers.lastName) + 1];
+    strcpy(lastName, pers.lastName);
+    middleInitial = pers.middleInitial;
+    title = new char [strlen(pers.title) + 1];
+    strcpy(title, pers.title);
+}
+
+Person::~Person()
+{
+    delete firstName;
+    delete lastName;
+    delete title;
+}
+
+void Person::ModifyTitle(const char *newTitle)
+{
+    delete title;  // delete old title
+    title = new char [strlen(newTitle) + 1];
+    strcpy(title, newTitle);
+}
+
+void Person::Print()
+{
+    cout << title << " " << firstName << " ";
+    cout << middleInitial << ". " << lastName << endl;
+}
+
+void Person::IsA()
+{
+    cout << "Person" << endl;
+}
+
+void Person::Greeting(const char *msg)
+{
+    cout << msg << endl;
+}
+
+class Student : public Person
+{
+private: 
+    // data members
+    float gpa;
+    char *currentCourse;
+    const char *studentId;  
+public:
+    // member function prototypes
+    Student();  // default constructor
+    Student(const char *, const char *, char, const char *,
+            float, const char *, const char *); 
+    Student(const Student &);  // copy constructor
+    virtual ~Student();  // destructor
+    void EarnPhD();  
+    // inline function definitions
+    float GetGpa() { return gpa; }
+    const char *GetCurrentCourse() { return currentCourse; }
+    const char *GetStudentId() { return studentId; }
+    void SetCurrentCourse(const char *); // prototype only
+  
+    // In the derived class, the keyword virtual is optional, 
+    // but recommended for internal documentation
+    virtual void Print();
+    virtual void IsA();
+    // note: we choose not to redefine 
+    // Person::Greeting(const char *)
+    virtual void Validate();
+};
+
+inline void Student::SetCurrentCourse(const char *c)
+{
+    delete currentCourse;   // delete existing course
+    currentCourse = new char [strlen(c) + 1];
+    strcpy(currentCourse, c); 
+}
+
+Student::Student() : studentId (0) 
+{
+    gpa = 0.0;
+    currentCourse = 0;
+}
+
+// Alternate constructor member function definition
+Student::Student(const char *fn, const char *ln, char mi, 
+                 const char *t, float avg, const char *course,
+                 const char *id) : Person(fn, ln, mi, t)
+{
+    gpa = avg;
+    currentCourse = new char [strlen(course) + 1];
+    strcpy(currentCourse, course);
+    char *temp = new char [strlen(id) + 1];
+    strcpy (temp, id); 
+    studentId = temp;
+}
+
+// Copy constructor definition
+Student::Student(const Student &ps) : Person(ps)
+{
+    gpa = ps.gpa;
+    currentCourse = new char [strlen(ps.currentCourse) + 1];
+    strcpy(currentCourse, ps.currentCourse);
+    char *temp = new char [strlen(ps.studentId) + 1];
+    strcpy (temp, ps.studentId); 
+    studentId = temp;
+}
+   
+// destructor definition
+Student::~Student()
+{
+    delete currentCourse;
+    delete (char *) studentId;
+}
+
+void Student::EarnPhD()
+{
+    ModifyTitle("Dr.");  
+}
+
+void Student::Print()
+{   // need to use access functions as these data members are
+    // defined in Person as private
+    cout << GetTitle() << " " << GetFirstName() << " ";
+    cout << GetMiddleInitial() << ". " << GetLastName();
+    cout << " with id: " << studentId << " GPA: ";
+    cout << setprecision(3) <<  " " << gpa;
+    cout << " Course: " << currentCourse << endl;
+}
+
+void Student::IsA()
+{
+    cout << "Student" << endl;
+}
+
+void Student::Validate()
+{
+    // check Student instance to see if standards are met; if not, throw an exception
+    throw "Student does not meet prerequisites"; 
+}
+
+
+int main()
+{
+    set_terminate(AppSpecificTerminate);  // register fn.
+
+    Student s1("Sara", "Kato", 'B', "Dr.", 3.9, "C++", "272PSU"); 
+
+    try    // ‘try’ this block of code -- Validate() may throw an exception
+    {      
+        s1.Validate();  // see if Student meets admission stds
+    }
+    catch (float err)
+    {
+        cout << err << endl;
+        // try to fix problem here…
+        exit(1); // only if you can’t fix, exit gracefully
+    } 
+    cout << "Moving onward with remainder of code." << endl;
+
+    return 0;
+}
+
