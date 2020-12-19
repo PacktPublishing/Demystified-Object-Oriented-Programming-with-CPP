55fe92b05a8e02074aa56be066fece0a511c2d4b
DorothyRKirk <64913825+DorothyRKirk@users.noreply.github.com>
2020-12-19 17:16:52 -0500

    Add files via upload

---
+++ Chapter15/Chp15-Ex1.cpp
@@ -0,0 +1,273 @@
+// (c) Dorothy R. Kirk. All Rights Reserved.
+// Purpose: To illustrate Canonical Class form in a hierarchy of related classes 
+// Explicit default constructor, copy constructor and overloaded assignment operators. And virtual destructor.
+
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+using namespace std;
+
+const int MAX = 5;
+
+class Person
+{
+private: 
+    // data members
+    char *firstName;
+    char *lastName;
+    char middleInitial;
+    char *title;  // Mr., Ms., Mrs., Miss, Dr., etc.
+protected:
+    void ModifyTitle(const char *); 
+public:
+    Person();   // default constructor
+    Person(const char *, const char *, char, const char *);  
+    Person(const Person &);  // copy constructor
+    virtual ~Person();  // virtual destructor
+
+    // inline function definitions
+    const char *GetFirstName() const { return firstName; }  
+    const char *GetLastName() const { return lastName; }    
+    const char *GetTitle() const { return title; } 
+    char GetMiddleInitial() const { return middleInitial; }
+
+    // Virtual functions will not be inlined since their 
+    // method must be determined at run time using v-table.
+    virtual void Print() const;
+    virtual void IsA();  
+    virtual void Greeting(const char *);
+
+    Person &operator=(const Person &);  // overloaded assignment operator prototype
+
+};
+
+Person::Person()
+{
+    firstName = lastName = 0;  // NULL pointer
+    middleInitial = '\0';
+    title = 0;
+}
+
+Person::Person(const char *fn, const char *ln, char mi, 
+               const char *t)
+{
+    firstName = new char [strlen(fn) + 1];
+    strcpy(firstName, fn);
+    lastName = new char [strlen(ln) + 1];
+    strcpy(lastName, ln);
+    middleInitial = mi;
+    title = new char [strlen(t) + 1];
+    strcpy(title, t);
+}
+
+// copy constructor
+Person::Person(const Person &pers)
+{
+    firstName = new char [strlen(pers.firstName) + 1];
+    strcpy(firstName, pers.firstName);
+    lastName = new char [strlen(pers.lastName) + 1];
+    strcpy(lastName, pers.lastName);
+    middleInitial = pers.middleInitial;
+    title = new char [strlen(pers.title) + 1];
+    strcpy(title, pers.title);
+}
+
+Person::~Person()
+{
+    delete firstName;
+    delete lastName;
+    delete title;
+}
+
+void Person::ModifyTitle(const char *newTitle)
+{
+    delete title;  // delete old title
+    title = new char [strlen(newTitle) + 1];
+    strcpy(title, newTitle);
+}
+
+void Person::Print() const
+{
+    if (title)
+       cout << title << " ";
+    if (firstName)
+       cout << firstName << " ";
+    else
+       cout << "No first name ";
+    if (middleInitial != '\0')
+       cout << middleInitial << ". ";
+    if (lastName)
+       cout << lastName << endl;
+    else
+       cout << "No last name" << endl;
+}
+
+void Person::IsA()
+{
+    cout << "Person" << endl;
+}
+
+void Person::Greeting(const char *msg)
+{
+    cout << msg << endl;
+}
+
+// overloaded assignment operator
+Person &Person::operator=(const Person &p)
+{
+   // make sure we're not assigning an object to itself
+   if (this != &p)
+   {
+      delete firstName;  // or call ~Person();
+      delete lastName;
+      delete title;
+
+      firstName = new char [strlen(p.firstName) + 1];
+      strcpy(firstName, p.firstName);
+      lastName = new char [strlen(p.lastName) + 1];
+      strcpy(lastName, p.lastName);
+      middleInitial = p.middleInitial;
+      title = new char [strlen(p.title) + 1];
+      strcpy(title, p.title);
+   }
+   return *this;  // allow for cascaded assignments
+}
+
+
+class Student : public Person
+{
+private: 
+    // data members
+    float gpa;
+    char *currentCourse;
+    const char *studentId;  
+public:
+    // member function prototypes
+    Student();  // default constructor
+    Student(const char *, const char *, char, const char *,
+            float, const char *, const char *); 
+    Student(const Student &);  // copy constructor
+    virtual ~Student();  // destructor
+    void EarnPhD();  
+    // inline function definitions
+    float GetGpa() const { return gpa; }
+    const char *GetCurrentCourse() const { return currentCourse; }
+    const char *GetStudentId() const { return studentId; }
+    void SetCurrentCourse(const char *); // prototype only
+  
+    // In the derived class, the keyword virtual is optional, 
+    // but recommended for internal documentation
+    virtual void Print() const override;
+    virtual void IsA() override;
+    // note: we choose not to redefine 
+    // Person::Greeting(const char *)
+    Student &operator=(const Student &);  // overloaded assignment operator prototype
+};
+
+inline void Student::SetCurrentCourse(const char *c)
+{
+    delete currentCourse;   // delete existing course
+    currentCourse = new char [strlen(c) + 1];
+    strcpy(currentCourse, c); 
+}
+
+Student::Student() : studentId (0) 
+{
+    gpa = 0.0;
+    currentCourse = 0;
+}
+
+// Alternate constructor member function definition
+Student::Student(const char *fn, const char *ln, char mi, 
+                 const char *t, float avg, const char *course,
+                 const char *id) : Person(fn, ln, mi, t)
+{
+    gpa = avg;
+    currentCourse = new char [strlen(course) + 1];
+    strcpy(currentCourse, course);
+    char *temp = new char [strlen(id) + 1];
+    strcpy (temp, id); 
+    studentId = temp;
+}
+
+// Copy constructor definition
+Student::Student(const Student &ps) : Person(ps)
+{
+    gpa = ps.gpa;
+    currentCourse = new char [strlen(ps.currentCourse) + 1];
+    strcpy(currentCourse, ps.currentCourse);
+    char *temp = new char [strlen(ps.studentId) + 1];
+    strcpy (temp, ps.studentId); 
+    studentId = temp;
+}
+   
+// destructor definition
+Student::~Student()
+{
+    delete currentCourse;
+    delete (char *) studentId;
+}
+
+void Student::EarnPhD()
+{
+    ModifyTitle("Dr.");  
+}
+
+void Student::Print() const
+{   // need to use access functions as these data members are
+    // defined in Person as private
+    cout << GetTitle() << " " << GetFirstName() << " ";
+    cout << GetMiddleInitial() << ". " << GetLastName();
+    cout << " with id: " << studentId << " GPA: ";
+    cout << setprecision(3) <<  " " << gpa;
+    cout << " Course: " << currentCourse << endl;
+}
+
+void Student::IsA()
+{
+    cout << "Student" << endl;
+}
+
+// overloaded assignment operator
+Student &Student::operator=(const Student &s)
+{
+   // make sure we're not assigning an object to itself
+   if (this != &s)
+   {
+      Person::operator=(s);  // call base class operator= for help
+
+      delete currentCourse;  // delete existing data members
+      delete studentId;
+
+      gpa = s.gpa;           // make deep assignment of data members
+      currentCourse = new char [strlen(s.currentCourse) + 1];
+      strcpy(currentCourse, s.currentCourse);
+      char *temp = new char [strlen(s.studentId) + 1];
+      strcpy (temp, s.studentId); 
+      studentId = temp;
+   }
+   return *this;  // allow for cascaded assignments
+}
+
+int main()
+{
+    Person *people[MAX];
+    people[0] = new Person("Juliet", "Martinez", 'M', "Ms.");
+    people[1] = new Student("Hana", "Sato", 'U', "Dr.", 3.8,
+                            "C++", "178PSU"); 
+    people[2] = new Student("Sara", "Kato", 'B', "Dr.", 3.9,
+                            "C++", "272PSU"); 
+    people[3] = new Person("Giselle", "LeBrun", 'R', "Miss");
+    people[4] = new Person("Linus", "Van Pelt", 'S', "Mr.");
+
+    for (int i = 0; i < MAX; i++)
+    {
+       people[i]->IsA();
+       cout << "  ";
+       people[i]->Print();
+    } 
+    for (int i = 0; i < MAX; i++)
+       delete people[i];   // engage virtual dest. sequence
+    return 0;
+}
+
---
+++ Chapter15/Chp15-Ex2.cpp
@@ -0,0 +1,58 @@
+// (c) Dorothy R. Kirk. All Rights Reserved.
+// Purpose: To illustrate items to include in a driver to test a class. 
+// Sample driver to test the Student class. 
+
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include "Person.h"
+#include "Student.h"
+
+using namespace std;
+
+const int MAX = 3;
+
+int main()
+{
+    // Test all means for instantiation, including copy constructor
+    Student s0; // Default construction
+
+    // instantiate two Students
+    Student s1("Jo", "Li", 'H', "Ms.", 3.7, "C++", "UD1234");
+    Student s2("Sam", "Lo", 'A', "Mr.", 3.5, "C++", "UD2245");
+
+    // These initializations implicitly invoke copy constructor
+    Student s3(s1);
+    Student s4 = s2;
+
+    // Test the assignment operator 
+    Student s5("Ren", "Ze", 'A', "Dr.", 3.8, "C++", "BU5563");
+    Student s6;
+    s6 = s5;  // this is an assignment, not initialization
+
+    // Test each public method. A sample is shown here. Be sure to test ALL methods.
+    s1.Print();   
+    s3.Print();
+    s2.Print();
+    s4.Print();
+   
+    // Generalize derived instances as base types -- do methods work as expected?
+    Person *people[MAX];
+    people[0] = new Person("Juliet", "Martinez", 'M', "Ms.");   // base instance for comparison
+    people[1] = new Student("Zack", "Moon", 'R', "Dr.", 3.8, "C++", "UMD1234");  // derived instances
+    people[2] = new Student("Gabby", "Doone", 'A', "Dr.", 3.9, "C++", "GWU4321"); 
+
+    for (int i = 0; i < MAX; i++)
+    {
+       people[i]->IsA();
+       cout << "  ";
+       people[i]->Print();
+    } 
+
+    // Test destruction sequence (for dynamically allocated instances)
+    for (int i = 0; i < MAX; i++)
+       delete people[i];   // engage virtual dest. sequence
+
+    return 0;
+}
+
