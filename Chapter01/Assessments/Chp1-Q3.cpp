bcbe89ec49f533d84e2f698d02f2248814d76a51
DorothyRKirk <64913825+DorothyRKirk@users.noreply.github.com>
2021-05-03 12:34:32 -0400

    Add files via upload

---
+++ Chapter01/Assessments/Chp1-Q3.cpp
@@ -0,0 +1,7 @@
+// Chapter 1, Question 3
+// Create a simple program using a struct or a class to make a user defined type 
+// for Book. Add data members for title, author, and number of pages. Create two 
+// variables of type BOok and use the dot operator . to fill in the data members
+// for each such instance. Use iostreams to both prompt the user for input values, and
+// to pring each Book instance when omplete. Use only features covered in this chapter.
+
