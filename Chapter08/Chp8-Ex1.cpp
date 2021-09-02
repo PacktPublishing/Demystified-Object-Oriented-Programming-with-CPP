017e75b327fe26f8e93f775ed5299765702aec09
DorothyRKirk <64913825+DorothyRKirk@users.noreply.github.com>
2021-09-02 16:01:22 -0400

    Add files via upload

--- Chapter08/Chp8-Ex1.cpp
+++ Chapter08/Chp8-Ex1.cpp
@@ -9,8 +9,9 @@ using std::cout;     // preferred to:  using namespace std;
 using std::endl;
 using std::setprecision;
 using std::string;
+using std::to_string;
 
-const int MAX = 5;
+constexpr int MAX = 5;
 
 // An abstract class is one which collects common traits of derived classes,
 // yet is does not itself represent a tangible entity, or an object which 
@@ -22,13 +23,15 @@ const int MAX = 5;
 class LifeForm   // abstract class definition
 {
 private:
-   int lifeExpectancy;
+   int lifeExpectancy = 0;  // in-class initialization
 public:
-   LifeForm() { lifeExpectancy = 0; }
-   LifeForm(int life) { lifeExpectancy = life; }
-   LifeForm(const LifeForm &form) 
-       { lifeExpectancy = form.lifeExpectancy; }
-   virtual ~LifeForm() { }      // virtual destructor
+   LifeForm() = default;    // use default ctor and in-class initialization
+   LifeForm(int life) : lifeExpectancy(life) { }
+   LifeForm(const LifeForm &) = default;
+   // If we wanted to write the copy constructor, this is what it would look like:
+   // LifeForm(const LifeForm &form) : lifeExpectancy(form.lifeExpectancy) { }   OR
+   // LifeForm(const LifeForm &form) { lifeExpectancy = form.lifeExpectancy; }
+   virtual ~LifeForm() = default;      // virtual destructor
    int GetLifeExpectancy() const { return lifeExpectancy; }
    virtual void Print() const = 0;   // pure virtual functions 
    virtual string IsA() const = 0;   
@@ -38,13 +41,13 @@ public:
 class Cat: public LifeForm
 {
 private:
-   int numberLivesLeft;
+   int numberLivesLeft = 9;   // in-class initialization
    string name;
 public:
-   Cat() : LifeForm(15), numberLivesLeft(9), name("") {  }
-   Cat(int lives) : LifeForm(15), numberLivesLeft(lives), name("") {  }
+   Cat() : LifeForm(15) { }  // remember, numberLivesLeft is set with in-class init and name will be an empty string
+   Cat(int lives) : LifeForm(15), numberLivesLeft(lives) {  }
    Cat(const string &);
-   virtual ~Cat() { }   // virtual destructor
+   ~Cat() override = default;    // virtual destructor
    const string &GetName() const { return name; }
    int GetNumberLivesLeft() const { return numberLivesLeft; }
    virtual void Print() const override;   // redefine pure virtual functions
@@ -52,7 +55,7 @@ public:
    virtual string Speak() const override { return "Meow!"; }
 };
 
-Cat::Cat(const string &n) : LifeForm(15), numberLivesLeft(9), name(n)
+Cat::Cat(const string &n) : LifeForm(15), name(n)  // numLivesLeft will be set to 9 with in-class init
 {
 }
 
@@ -68,25 +71,29 @@ private:
    // data members
    string firstName;
    string lastName;
-   char middleInitial;
+   char middleInitial = '\0';  // in-class initialization
    string title;  // Mr., Ms., Mrs., Miss, Dr., etc.
 protected:
    void ModifyTitle(const string &);  
 public:
-   Person();   // default constructor
+   Person();   // programmer supplied default constructor
    Person(const string &, const string &, char, const string &);  
-   Person(const Person &);  // copy constructor
-   virtual ~Person();  // destructor
+   Person(const Person &) = default;  // copy constructor
+   ~Person() override = default;  // destructor
    const string &GetFirstName() const { return firstName; }  
    const string &GetLastName() const { return lastName; }    
    const string &GetTitle() const { return title; } 
    char GetMiddleInitial() const { return middleInitial; }
-   virtual void Print() const override;  // redefine pure virtual functions
-   virtual string IsA() const override;   
-   virtual string Speak() const override;
+   void Print() const override;  // redefine pure virtual functions
+   string IsA() const override;   
+   string Speak() const override;
 };
 
-Person::Person() : LifeForm(80), firstName(""), lastName(""), middleInitial('\0'), title("")
+
+// With in-class initialization, we don't need to set middleInitial.  
+// Remember, string members are automatically initialized to empty with the default string constructor
+// However, we will select the LifeForm(int) constructor in the member init list to allow us to pass a lifexpectancy of 80
+Person::Person() : LifeForm(80)
 {
 }
 
@@ -95,14 +102,14 @@ Person::Person(const string &fn, const string &ln, char mi, const string &t) :
 {
 }
 
+// We don't need to write the copy constructor ourselves (there are no ptr data members in class).
+// If we did choose to do so, this is how it would look:
+/*
 Person::Person(const Person &p) : LifeForm(p), firstName(p.firstName), lastName(p.lastName), 
                                                middleInitial(p.middleInitial), title(p.title)
 {
 }
-
-Person::~Person()
-{
-}
+*/
 
 void Person::ModifyTitle(const string &newTitle)
 {
@@ -129,49 +136,71 @@ class Student : public Person
 {
 private: 
    // data members
-   float gpa;
+   float gpa = 0.0;    // in-class initialization
    string currentCourse;
    const string studentId;  
+   static int numStudents;
 public:
    Student();  // default constructor
-   Student(const string &, const string &, char, const string &,
-           float, const string &, const string &); 
+   Student(const string &, const string &, char, const string &, float, const string &, const string &); 
    Student(const Student &);  // copy constructor
-   virtual ~Student();  // destructor
+   ~Student() override;  // destructor
    void EarnPhD();  
    float GetGpa() const { return gpa; }
    const string &GetCurrentCourse() const { return currentCourse; }
    const string &GetStudentId() const { return studentId; }
    void SetCurrentCourse(const string &);
-   virtual void Print() const final override; // redefine not all virtual functions (and mark Print as the final override)
-   virtual string IsA() const override;
+   void Print() const final override; // redefine not all virtual functions (and mark Print as the final override)
+   string IsA() const override;
+   static int GetNumberStudents(); // static member function
 };
+
+// definition for static data member (which is implemented as external variable)
+int Student::numStudents = 0;  // notice initial value of 0
 
 inline void Student::SetCurrentCourse(const string &c)
 {
    currentCourse = c;
 }
 
-Student::Student() : gpa(0.0), currentCourse(""), studentId ("")
+// Definition for static member function (it is also inline)
+inline int Student::GetNumberStudents()
 {
-   // It would be a better idea to initialize studentId to a random id (since it is const and cannot be later reset)
+    return numStudents;
 }
+
+// Notice that data members using in-class initialization (above), will be set for those members not in the member init list.
+// However, those that can not be easily set with in-class initialization (such as static numStudents), we set below in method.
+// Recall that member objects (strings) will be default constructed, so no additional init is necessary (if an empty string is our goal)
+Student::Student() : studentId(to_string(numStudents + 100) + "Id")
+{
+   // Note: since studentId is const, we need to set it at construction. We're doing so in member init list with
+   // a unique id (based upon numStudents counter + 100), concatenated with the string "Id" .
+   // Remember, string member currentCourse will be default constructed with an empty string - it is a member object
+   // Also, remember to dynamically allocate memory for any pointer data members here (not needed in this example)
+   numStudents++;
+}
+
+
 
 // Alternate constructor member function definition
 Student::Student(const string &fn, const string &ln, char mi, const string &t,
                  float avg, const string &course, const string &id) : 
                  Person(fn, ln, mi, t), gpa(avg), currentCourse(course), studentId(id)
 {
+   numStudents++;
 }
 
 // Copy constructor definition
 Student::Student(const Student &s) : Person(s), gpa(s.gpa), currentCourse(s.currentCourse), studentId(s.studentId)
 {
+   numStudents++;
 }
    
-// destructor definition
+// destructor definition. We have additional work to do so we can't just accept the default implementation
 Student::~Student()
 {
+   numStudents--;
 }
 
 void Student::EarnPhD()
@@ -184,7 +213,7 @@ void Student::Print() const
    cout << "\t" << GetTitle() << " " << GetFirstName() << " ";
    cout << GetMiddleInitial() << ". " << GetLastName();
    cout << " id: " << studentId << "\n\twith gpa: ";   // tab \t followed by word 'with' (no space purposefully)
-   cout << setprecision(2) <<  " " << gpa << " enrolled in: ";
+   cout << setprecision(3) <<  " " << gpa << " enrolled in: ";
    cout << currentCourse << endl;
 }
 
