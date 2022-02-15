c3346d1507154382723be97f9f36e6b3d3b6289c
DorothyRKirk <64913825+DorothyRKirk@users.noreply.github.com>
2022-02-15 14:39:53 -0500

    Add files via upload

---
+++ Chapter08/Chp7-Ex1.cpp
@@ -0,0 +1,227 @@
+// (c) Dorothy R. Kirk. All Rights Reserved.
+// Purpose: To illustrate polymorphism in C++ with virtual functions.
+
+#include <iostream>
+#include <iomanip>
+
+using std::cout;    //preferred to: using namespace std;
+using std::endl;
+using std::setprecision;
+using std::string;
+using std::to_string;
+
+constexpr int MAX = 5;
+
+class Person
+{
+private: 
+    // data members
+    string firstName;
+    string lastName;
+    char middleInitial = '\0';  // in-class initialization -- value to be used in default constructor
+    string title;  // Mr., Ms., Mrs., Miss, Dr., etc.
+protected:
+    void ModifyTitle(const string &); 
+public:
+    Person() = default;   // default constructor
+    Person(const string &, const string &, char, const string &);  
+    // The copy constructor prototype with =default is unnecessary - we get this by automatically. Hence, it is commented out.
+    // Person(const Person &) = default;  // copy constructor
+    virtual ~Person();   // virtual destructor
+
+    // inline function definitions
+    const string &GetFirstName() const { return firstName; }    // firstName returned as const string 
+    const string &GetLastName() const { return lastName; }      // so is lastName (via implicit cast)
+    const string &GetTitle() const { return title; } 
+    char GetMiddleInitial() const { return middleInitial; }
+
+    // Virtual functions will not be inlined since their method must be determined at run time using v-table.
+    virtual void Print() const;
+    virtual void IsA() const;  
+    virtual void Greeting(const string &) const;
+};
+
+// With in-class initialization, writing the default constructor yourself is no longer necessary
+// Here's how it would look if you did choose to provide one (and also chose not to use in-class initialization)
+/*
+Person::Person() : middleInitial('\0')
+{
+   // Remember, string members are automatically initialized to empty with the default string constructor
+   // dynamically allocate memory for any pointer data members here
+}
+*/
+
+Person::Person(const string &fn, const string &ln, char mi, const string &t) :
+               firstName(fn), lastName(ln), middleInitial(mi), title(t)
+{
+   // dynamically allocate memory for any pointer data members here
+}
+
+// We are using default copy constructor, but if you needed to write it yourself, here is what it would look like:
+// Remember to prototype it too!
+/*
+Person::Person(const Person &p) :
+               firstName(p.firstName), lastName(p.lastName),
+               middleInitial(p.middleInitial), title(p.title)
+{
+   // deep copy any pointer data members here
+}
+*/
+
+// Simple destructor written ourselves, just so we can trace the destructor chain using cout statements 
+Person::~Person()
+{
+   // release memory for any dynamically allocated data members
+   cout << "Person destructor <" << firstName << " " << lastName << ">" << endl;
+}
+
+void Person::ModifyTitle(const string &newTitle)
+{
+   title = newTitle;     // assignment between strings ensures a deep assignment
+}
+
+void Person::Print() const
+{
+    cout << title << " " << firstName << " ";
+    cout << middleInitial << ". " << lastName << endl;
+}
+
+void Person::IsA() const
+{
+    cout << "Person" << endl;
+}
+
+void Person::Greeting(const string &msg) const
+{
+    cout << msg << endl;
+}
+
+class Student : public Person
+{
+private: 
+    // data members
+    float gpa = 0.0;    // in-class initializataion
+    string currentCourse;
+    const string studentId;  
+    static int numStudents;  // static data member is initialized outside of class (see below)
+public:
+    // member function prototypes
+    Student();  // default constructor
+    Student(const string &, const string &, char, const string &, float, const string &, const string &); 
+    Student(const Student &);  // copy constructor
+    ~Student() override;  // This is a virtual destructor; override indicates 'entry' point in destruction sequence 
+    void EarnPhD();  
+    // inline function definitions
+    float GetGpa() const { return gpa; }
+    const string &GetCurrentCourse() const { return currentCourse; }
+    const string &GetStudentId() const { return studentId; }
+    void SetCurrentCourse(const string &); // prototype only
+  
+    // In the derived class, the keyword virtual is optional for overridden (polymorphic) methods, as is the keyword "override"
+    // Currently, "override" is recommended for internal documentation, however "virtual" is not recommended
+    void Print() const final override;  // final indicates this may not be overridden beyond Student
+    void IsA() const override;
+    // note: we choose not to redefine Person::Greeting(const string &) const
+
+    static int GetNumberStudents(); // static member function
+};
+
+// definition for static data member (which is implemented as external variable)
+int Student::numStudents = 0;  // notice initial value of 0
+
+inline void Student::SetCurrentCourse(const string &c)
+{
+   currentCourse = c;
+}
+
+// Definition for static member function (it is also inline)
+inline int Student::GetNumberStudents()
+{
+    return numStudents;
+}
+
+// Notice that data members using in-class initialization (above), will be set for those members not in the member init list.
+// However, those that can not be easily set with in-class initialization (such as static numStudents), we set below in method.
+// Recall that member objects (strings) will be default constructed, so no additional init is necessary (if an empty string is our goal)
+Student::Student() : studentId(to_string(numStudents + 100) + "Id")
+{
+   // Note: since studentId is const, we need to set it at construction. We're doing so in member init list with
+   // a unique id (based upon numStudents counter + 100), concatenated with the string "Id" .
+   // Remember, string member currentCourse will be default constructed with an empty string - it is a member object
+   // Also, remember to dynamically allocate memory for any pointer data members here (not needed in this example)
+   numStudents++;
+}
+
+
+// Alternate constructor member function definition
+Student::Student(const string &fn, const string &ln, char mi, const string &t,
+       float avg, const string &course, const string &id) : Person(fn, ln, mi, t),
+                       gpa(avg), currentCourse(course), studentId(id)
+{
+   // dynamically allocate memory for any pointer data members here
+   numStudents++;
+}
+
+// Copy constructor definition
+Student::Student(const Student &s) : Person(s),
+                 gpa(s.gpa), currentCourse(s.currentCourse), studentId(s.studentId)
+{
+   // deep copy any pointer data members of derived class here
+   numStudents++;
+
+}
+
+// destructor definition
+Student::~Student()
+{
+   // release memory for any dynamically allocated data members
+   numStudents--;
+   cout << "Student destructor <" << GetFirstName() << " " << GetLastName() << ">" << endl;
+}
+
+void Student::EarnPhD()
+{
+    ModifyTitle("Dr.");  
+}
+
+void Student::Print() const
+{   // need to use access functions as these data members are
+    // defined in Person as private
+    cout << GetTitle() << " " << GetFirstName() << " ";
+    cout << GetMiddleInitial() << ". " << GetLastName();
+    cout << " with id: " << studentId << " GPA: ";
+    cout << setprecision(3) <<  " " << gpa;
+    cout << " Course: " << currentCourse << endl;
+}
+
+void Student::IsA() const
+{
+    cout << "Student" << endl;
+}
+
+int main()
+{
+    Person *people[MAX] = { };  // will be initialized to nullptrs
+
+    people[0] = new Person("Juliet", "Martinez", 'M', "Ms.");
+    people[1] = new Student("Hana", "Sato", 'U', "Dr.", 3.8,
+                            "C++", "178PSU"); 
+    people[2] = new Student("Sara", "Kato", 'B', "Dr.", 3.9,
+                            "C++", "272PSU"); 
+    people[3] = new Person("Giselle", "LeBrun", 'R', "Miss");
+    people[4] = new Person("Linus", "Van Pelt", 'S', "Mr.");
+
+    // We will soon see a more modern way to use for loops (range for)
+    for (int i = 0; i < MAX; i++)
+    {
+       people[i]->IsA();
+       cout << "  ";
+       people[i]->Print();
+    } 
+
+    for (int i = 0; i < MAX; i++)
+       delete people[i];   // engage virtual dest. sequence
+
+    return 0;
+}
+
